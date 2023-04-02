6c9008c1730b6a4ec37387a1a68ea6c662c4e823
Hennadii Borysevych <genuch3@gmail.com>
2023-04-02 22:27:54 +0300

    first and second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import user from 'userData/user.json';
+import stats from 'userData/stats.json';
+import { Profile } from 'components/profile/Profile.jsx';
+import { Statistics } from 'components/statistics/Statistics.jsx';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        id={stats.id}
+        label={stats.label}
+        percentage={stats.percentage}
+      />
+    </>
   );
 };
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+export const Statistics = ({ title, id, label, percentage }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">{title}</h2>
+
+      <ul class="stat-list">
+        <li class="item">
+          <span class="label">.docx</span>
+          <span class="percentage">4%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp3</span>
+          <span class="percentage">14%</span>
+        </li>
+        <li class="item">
+          <span class="label">.pdf</span>
+          <span class="percentage">41%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp4</span>
+          <span class="percentage">12%</span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+// [
+//   { "id": "id-1", "label": ".docx", "percentage": 22 },
+//   { "id": "id-2", "label": ".pdf", "percentage": 4 },
+//   { "id": "id-3", "label": ".mp3", "percentage": 17 },
+//   { "id": "id-4", "label": ".psd", "percentage": 47 },
+//   { "id": "id-5", "label": ".pdf", "percentage": 10 }
+// ]
